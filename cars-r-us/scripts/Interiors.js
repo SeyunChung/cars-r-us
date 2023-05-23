da25cec19aab301db5421ca60ae5591dfec0a7b3
Seyun Chung <119281933+SeyunChung@users.noreply.github.com>
2023-05-23 10:52:05 -0500

    Update Interiors.js

--- cars-r-us/scripts/Interiors.js
+++ cars-r-us/scripts/Interiors.js
@@ -7,9 +7,9 @@ const changeHandler = (changeEvent) => {
     }
  }
 document.addEventListener("change", changeHandler)
-//create html for displaying the paints menu
+//create html for displaying the interiors menu
 export const Interiors = async () => {
-    //fetch the paints data from our local api
+    //fetch the interiors data from our local api
     const response = await fetch("http://localhost:8088/interiors")
     const interiors = await response.json() 
 
